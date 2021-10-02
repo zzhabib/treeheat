dda78ce1bc6b0a2688ea052bfa791598be8453c6
Matthew <matt.jurenka@comcast.net>
2021-10-02 11:32:33 -0700

    added map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import GoogleMapReact from "google-map-react"
 
 
 const IndexPage = () => {
-    console.log(process.env.GOOGLE_MAPS_KEY)
-    return <h4>Hello World</h4>
+    return <>
+        <div style={{width: 1000, height: 600}}>
+            <GoogleMapReact
+                bootstrapURLKeys={{
+                    key: process.env.GOOGLE_MAPS_KEY as string
+                }}
+                defaultCenter={{
+                    lat: 59.95,
+                    lng: 30.33
+                }}
+                defaultZoom={11}
+            />
+        </div>
+    </>
 }
 
 export default IndexPage

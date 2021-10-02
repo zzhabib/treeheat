ae0b2fa3e559935a50a520cf159627191bcb7e40
Matthew <matt.jurenka@comcast.net>
2021-10-02 12:03:16 -0700

    added trees

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,19 @@
-import * as React from "react"
+import React, { useState } from "react"
 import GoogleMapReact from "google-map-react"
 
 
+type Coord = {
+    lat: number
+    lng: number
+}
+
+const TreeIcon = (_props: { lat: number, lng: number }) => <div
+    style={{ width: 25, height: 25, backgroundColor: "green" }}
+></div>
+
 const IndexPage = () => {
+    const [trees, set_trees] = useState<Coord[]>([])
+
     return <>
         <div style={{width: 1000, height: 600}}>
             <GoogleMapReact
@@ -10,11 +21,17 @@ const IndexPage = () => {
                     key: process.env.GOOGLE_MAPS_KEY as string
                 }}
                 defaultCenter={{
-                    lat: 59.95,
-                    lng: 30.33
+                    lat: 33.4484,
+                    lng: -112.0740
                 }}
                 defaultZoom={11}
-            />
+                onClick={({lat, lng}) => {
+                    set_trees(trees.concat([{lat, lng}]))
+                }}
+                zoom={11}
+            >
+                {trees.map((tree, idx) => <TreeIcon key={idx} lat={tree.lat} lng={tree.lng}/>)}
+            </GoogleMapReact>
         </div>
     </>
 }

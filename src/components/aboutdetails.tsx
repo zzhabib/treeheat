c7b724004c1685c647987ba56688e827f535c075
Zachary Habib <zach.habib16@gmail.com>
2021-10-02 14:44:36 -0700

    Added info on About page

--- src/components/aboutdetails.tsx
+++ src/components/aboutdetails.tsx
@@ -4,14 +4,23 @@ const style = {
     margin: "auto",
 }
 
+//References
+//https://trees-energy-conservation.extension.org/how-do-trees-cool-the-air/
+//https://enviroatlas.epa.gov/enviroatlas/DataFactSheets/pdf/Supplemental/Percenttreecoveralongwalkableroads.pdf
+//https://www.usda.gov/media/blog/2015/03/17/power-one-tree-very-air-we-breathe
+
 const AboutDetails = () => {
     return (
         <div style={style}>
             <h1>About</h1>
             <h2>What is Tree Heat?</h2>
-            <p>Tree Heat is an educational tool used to convey the relationship between the
-                population of trees and surface temperature in a city.</p>
+            <p>Tree Heat is an interactive map that allows users to explore the temperature benefit
+                that trees bring to its surrounding environment.</p>
             <h2>Why trees?</h2>
+            <p>Trees are able to cool the air around them by blocking harsh sunlight from reaching the surrounding surface.
+                In addition, trees also absorb carbon dioxide from the atmosphere along with light through a process commonly
+                known as photosynthesis.
+            </p>
             <h2>How does Tree Heat work?</h2>
             <h2></h2>
         </div>

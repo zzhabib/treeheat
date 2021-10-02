d73fcf2f9ca98d85f3cf9becb91f754b782446cb
Matthew <matt.jurenka@comcast.net>
2021-10-02 10:14:45 -0700

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+
+const IndexPage = () => (
+    <h4>Hello World</h4>
+)
+
+export default IndexPage
